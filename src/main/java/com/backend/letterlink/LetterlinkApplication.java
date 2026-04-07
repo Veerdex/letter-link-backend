cadd9265c3e7796329003da364a40e1e0824d3c5
Veerdex <grantkoupal@u.boisestate.edu>
2026-04-06 22:45:09 -0600

    Initial commit

---
+++ src/main/java/com/backend/letterlink/LetterlinkApplication.java
@@ -0,0 +1,11 @@
+package com.backend.letterlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterlinkApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LetterlinkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/backend/letterlink/LetterlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.letterlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
